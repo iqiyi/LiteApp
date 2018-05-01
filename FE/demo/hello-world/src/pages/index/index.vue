3684816b57af9eeea901afffc22778ae9105d08d
pricelesss <454909522@qq.com>
2018-05-01 11:13:08 +0800

    remove src/ from .gitignore

---
+++ FE/demo/hello-world/src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>
+            hello world~
+        </h1>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.container{
+    width:100%;
+    padding:0 50px;
+    background:green;
+    h1{
+        font-size:20px;
+        font-weight:600;
+        color:orange;
+    }
+}
+</style>
